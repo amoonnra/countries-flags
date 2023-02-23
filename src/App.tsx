d39a1072551fa37bddb404f97b9873a7d992fa8d
amoonra <dvoeglazov@duck.com>
2023-02-23 19:13:24 +0500

    fix gh pages

--- src/App.tsx
+++ src/App.tsx
@@ -14,7 +14,7 @@ function App() {
 			<Main>
 				<Routes>
 					<Route path='/countries-flags/' element={<HomePage />} />
-					<Route path='/countries-flags//country/:name' element={<Details />} />
+					<Route path='/countries-flags/country/:name' element={<Details />} />
 					<Route path='*' element={<NotFound />} />
 				</Routes>
 			</Main>
